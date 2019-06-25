0687bf0f1016fb8371a55d0e3a7cac425043cad4
lehui99 <lehui99@gmail.com>
2019-06-25 17:31:45 +0800

    Updates

--- doc/signatures/upload_process/main.py
+++ doc/signatures/upload_process/main.py
@@ -3,7 +3,7 @@ import sqlite3
 
 
 def main():
-    dbconn = sqlite3.connect('data\\signature.db')
+    dbconn = sqlite3.connect('upload\\signature.db')
     c = dbconn.cursor()
     c.execute('SELECT id, full_name, account, telno, addr, filename, dt FROM signature')
     with open('processed\\index.html', 'w', encoding='utf-8') as html_file:
@@ -14,10 +14,19 @@ def main():
 </head>
 <body>
 <table border="1" width="100%">
+<tr>
+<td>id</td>
+<td>Full name</td>
+<td>TDW account</td>
+<td>Tel No.</td>
+<td>Area address</td>
+<td>Original filename</td>
+<td>Upload time</td>
+</tr>
 ''')
         for row in c.fetchall():
             file_ext = row[5][-4:].lower()
-            shutil.copy('data\\signature_pic_%d' % row[0], 'processed\\signature_pic_%d%s' % (row[0], file_ext))
+            shutil.copy('upload\\signature_pic_%d' % row[0], 'processed\\signature_pic_%d%s' % (row[0], file_ext))
             html_file.write('''<tr>\n''')
             html_file.write('''<td>%d</td>\n''' % row[0])
             html_file.write('''<td>%s</td>\n''' % row[1])
